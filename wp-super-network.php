2d683c99246cb43be64fee96745cbc3fa4597a16
Carlo Manfredi <carlomethod@gmail.com>
2022-08-09 11:43:32 +1000

    v1.2.0-beta1

--- wp-super-network.php
+++ wp-super-network.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Super Network
  * Plugin URI:
  * Description: Share content between sites and create offspring networks.
- * Version: 1.1.0
+ * Version: 1.2.0-beta1
  * Requires at least: 5.0
  * Requires PHP: 5.6
  * Author: Ask Carlo

6c12f5902fd83fee2e4720791ec8b1e0360da048
carlomanf <carlomanf@users.noreply.github.com>
2026-06-19 08:41:20 +0000

    Intercept attachment links

--- src/permalink_transformer.php
+++ src/permalink_transformer.php
@@ -6,89 +6,133 @@ namespace WP_Super_Network;
 
 class Permalink_Transformer
 {
-    /**
-     * Network instance.
-     *
-     * @since 1.3.0
-     * @var Network
-     */
-    private $network;
+	/**
+	 * Network instance.
+	 *
+	 * @since 1.3.0
+	 * @var Network
+	 */
+	private $network;
 
-    /**
-     * Constructor.
-     *
-     * @since 1.3.0
-     *
-     * @param Network $network Network instance.
-     */
-    public function __construct( $network )
-    {
-        $this->network = $network;
-    }
+	/**
+	 * Constructor.
+	 *
+	 * @since 1.3.0
+	 *
+	 * @param Network $network Network instance.
+	 */
+	public function __construct( $network )
+	{
+		$this->network = $network;
+	}
 
-    /**
-     * Intercept permalink for posts, pages, taxonomy archives, and attachment source links.
-     *
-     * @since 1.3.0
-     *
-     * @param string $permalink The original permalink.
-     * @param int $post_ID The post ID.
-     * @return string The transformed permalink.
-     */
-    public function intercept_permalink( $permalink, $post_ID )
-    {
-        if ( !doing_filter( 'supernetwork_preview_link' ) && !is_null( $blog = $this->network->get_blog( $post_ID ) ) ) {
-            switch_to_blog( $blog->id );
-            $permalink = get_permalink( $post_ID );
-            restore_current_blog();
-        }
+	/**
+	 * Intercept permalink for posts, pages, taxonomy archives, and attachment source links.
+	 *
+	 * @since 1.3.0
+	 *
+	 * @param string $permalink The original permalink.
+	 * @param int $post_ID The post ID.
+	 * @return string The transformed permalink.
+	 */
+	public function intercept_permalink( $permalink, $post_ID )
+	{
+		if ( !doing_filter( 'supernetwork_preview_link' ) && !is_null( $blog = $this->network->get_blog( $post_ID ) ) ) {
+			switch_to_blog( $blog->id );
+			$permalink = get_permalink( $post_ID );
+			restore_current_blog();
+		}
 
-        return $permalink;
-    }
+		return $permalink;
+	}
 
-    /**
-     * Intercept permalink for posts.
-     *
-     * @since 1.3.0
-     *
-     * @param string $permalink The original permalink.
-     * @param WP_Post $post The post object.
-     * @return string The transformed permalink.
-     */
-    public function intercept_permalink_for_post( $permalink, $post )
-    {
-        return $this->intercept_permalink( $permalink, $post->ID );
-    }
+	/**
+	 * Intercept permalink for posts.
+	 *
+	 * @since 1.3.0
+	 *
+	 * @param string $permalink The original permalink.
+	 * @param WP_Post $post The post object.
+	 * @return string The transformed permalink.
+	 */
+	public function intercept_permalink_for_post( $permalink, $post )
+	{
+		return $this->intercept_permalink( $permalink, $post->ID );
+	}
 
-    /**
-     * Intercept preview link.
-     *
-     * @since 1.3.0
-     *
-     * @param string $preview_link The original preview link.
-     * @param WP_Post $post The post object.
-     * @return string The transformed preview link.
-     */
-    public function intercept_preview_link( $preview_link, $post )
-    {
-        return doing_filter( 'supernetwork_preview_link' ) ? $preview_link : apply_filters( 'supernetwork_preview_link', $preview_link, $post );
-    }
+	/**
+	 * Intercept preview link.
+	 *
+	 * @since 1.3.0
+	 *
+	 * @param string $preview_link The original preview link.
+	 * @param WP_Post $post The post object.
+	 * @return string The transformed preview link.
+	 */
+	public function intercept_preview_link( $preview_link, $post )
+	{
+		return doing_filter( 'supernetwork_preview_link' ) ? $preview_link : apply_filters( 'supernetwork_preview_link', $preview_link, $post );
+	}
 
-    /**
-     * Replace preview link.
-     *
-     * @since 1.3.0
-     *
-     * @param string $preview_link The original preview link.
-     * @param WP_Post $post The post object.
-     * @return string The transformed preview link.
-     */
-    public function replace_preview_link( $preview_link, $post )
-    {
-        $query_args = array();
-        parse_str( parse_url( $preview_link, PHP_URL_QUERY ), $query_args );
-        return get_preview_post_link( $post, array_intersect_key( $query_args, array( 'preview_nonce' => null, 'preview_id' => null ) ) );
-    }
+	/**
+	 * Replace preview link.
+	 *
+	 * @since 1.3.0
+	 *
+	 * @param string $preview_link The original preview link.
+	 * @param WP_Post $post The post object.
+	 * @return string The transformed preview link.
+	 */
+	public function replace_preview_link( $preview_link, $post )
+	{
+		$query_args = array();
+		parse_str( parse_url( $preview_link, PHP_URL_QUERY ), $query_args );
+		return get_preview_post_link( $post, array_intersect_key( $query_args, array( 'preview_nonce' => null, 'preview_id' => null ) ) );
+	}
+
+	public function intercept_attachment_url( $url, $attachment_id )
+	{
+		if ( !is_null( $blog = $this->network->get_blog( $attachment_id ) ) )
+		{
+			switch_to_blog( $blog->id );
+			$url = wp_get_attachment_url( $attachment_id );
+			restore_current_blog();
+		}
+
+		return $url;
+	}
+
+	public function intercept_attached_file( $file, $attachment_id )
+	{
+		if ( !is_null( $blog = $this->network->get_blog( $attachment_id ) ) )
+		{
+			switch_to_blog( $blog->id );
+			$file = get_attached_file( $attachment_id );
+			restore_current_blog();
+		}
+
+		return $file;
+	}
+
+	public function intercept_srcset_meta( $image_meta, $size_array, $image_src, $attachment_id )
+	{
+		if ( !is_null( $blog = $this->network->get_blog( $attachment_id ) ) )
+		{
+			switch_to_blog( $blog->id );
+		}
+
+		return $image_meta;
+	}
+
+	public function intercept_srcset( $sources, $size_array, $image_src, $image_meta, $attachment_id )
+	{
+		if ( $this->network->get_blog( $attachment_id ) !== null )
+		{
+			restore_current_blog();
+		}
+
+		return $sources;
+	}
 
 	public function intercept_capability( $allcaps, $caps, $args, $user )
 	{
@@ -122,18 +166,22 @@ class Permalink_Transformer
 		return $allcaps;
 	}
 
-    /**
-     * Register all add_filter calls.
-     *
-     * @since 1.3.0
-     */
-    public function register_filters()
-    {
-        add_filter( 'post_type_link', array( $this, 'intercept_permalink_for_post' ), 10, 2 );
-        add_filter( 'post_link', array( $this, 'intercept_permalink_for_post' ), 10, 2 );
-        add_filter( 'page_link', array( $this, 'intercept_permalink' ), 10, 2 );
-        add_filter( 'preview_post_link', array( $this, 'intercept_preview_link' ), 10, 2 );
-        add_filter( 'supernetwork_preview_link', array( $this, 'replace_preview_link' ), 10, 2 );
+	/**
+	 * Register all add_filter calls.
+	 *
+	 * @since 1.3.0
+	 */
+	public function register_filters()
+	{
+		add_filter( 'post_type_link', array( $this, 'intercept_permalink_for_post' ), 10, 2 );
+		add_filter( 'post_link', array( $this, 'intercept_permalink_for_post' ), 10, 2 );
+		add_filter( 'page_link', array( $this, 'intercept_permalink' ), 10, 2 );
+		add_filter( 'preview_post_link', array( $this, 'intercept_preview_link' ), 10, 2 );
+		add_filter( 'supernetwork_preview_link', array( $this, 'replace_preview_link' ), 10, 2 );
+		add_filter( 'wp_get_attachment_url', array( $this, 'intercept_attachment_url' ), 10, 2 );
+		add_filter( 'get_attached_file', array( $this, 'intercept_attached_file' ), 10, 2 );
+		add_filter( 'wp_calculate_image_srcset_meta', array( $this, 'intercept_srcset_meta' ), 10, 4 );
+		add_filter( 'wp_calculate_image_srcset', array( $this, 'intercept_srcset' ), 10, 5 );
 		add_filter( 'user_has_cap', array( $this, 'intercept_capability' ), 10, 4 );
-    }
+	}
 }
